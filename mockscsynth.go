35a7ae70f8f4cdc0fb1afc11adb7aa0048fc4ae9
Brian Sorahan <bsorahan@gmail.com>
2017-03-26 09:49:47 -0500

    initial commit

---
+++ mockscsynth.go
@@ -0,0 +1,40 @@
+// Package mockscsynth provides an OSC server that can mock
+// scsynth, the SuperCollider server component.
+package mockscsynth
+
+import (
+	"net"
+	"testing"
+
+	"github.com/scgolang/osc"
+)
+
+func New(t *testing.T, listenAddr string) osc.Conn {
+	udpAddr, err := net.ResolveUDPAddr("udp", listenAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn, err := osc.ListenUDP("udp", udpAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	synthdefDoneMsg := osc.Message{
+		Address: "/done", // TODO: reuse constants from sc
+		Arguments: osc.Arguments{
+			osc.String("/d_recv"),
+		},
+	}
+	go func() {
+		if err := conn.Serve(1, osc.Dispatcher{
+			"/d_recv": osc.Method(func(m osc.Message) error {
+				return conn.SendTo(m.Sender, synthdefDoneMsg)
+			}),
+			"/g_new": osc.Method(func(m osc.Message) error {
+				return nil
+			}),
+		}); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	return conn
+}
